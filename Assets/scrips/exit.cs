94c2408bed0d6633c067ce86fe02f59f8024de1e
Jesús Céspedes Sobrao <35533077+meowchus@users.noreply.github.com>
2018-06-05 10:34:02 +0200

    tablas completas

---
+++ Assets/scrips/exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+
+    public void OnMouseDown()
+    {
+            Application.Quit();
+            Debug.Log("hasta pronto salio del juego");
+        
+    }
+}
---
+++ Assets/scrips/tablademultiplicar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tablademultiplicar : MonoBehaviour {
+
+	public Slider SlA;
+	public Text resultados;
+	int A,prov;
+	
+	public void multiplicar(){
+		A= (int)SlA.value;
+			resultados.text= "Resultados:";
+			for (int indice=0;indice<=10;indice++){
+				prov=A*indice;
+				resultados.text+=""+"\n"+""+A+"*"+indice+"="+prov;
+			}
+	}
+
+
+
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
